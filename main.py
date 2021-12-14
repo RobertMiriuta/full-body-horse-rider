53a201e75cb171cd49dda62caf48bf93e028503e
Robert <robert.miriuta@googlemail.com>
2021-12-14 17:43:07 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import mediapipe as mp
+import cv2
+import time
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+with mp_holistic.Holistic(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+) as holistic:
+
+    while cap.isOpened():
+
+        success, image = cap.read()
+        image = cv2.resize(image, (1160, 720))
+
+        start = time.time()
+
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+        image.flags.writeable = False
+        results = holistic.process(image)
+        image.flags.writeable = True
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        #print(results.pose_landmarks)
+        #mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        #mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+
+        end = time.time()
+        totalTime = end - start
+
+        fps = 1 / totalTime
+        print("FPS: ", fps)
+
+        cv2.putText(image, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 2)
+        cv2.imshow('MediaPipe Holistic', image)
+
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+
+cap.release()
+
+
